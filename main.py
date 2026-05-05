3453691fd260e3218988540de9974dc01397d7b1
Dan_Wu <wudan5339@gmail.com>
2026-05-05 13:08:59 +0200

    add-branch2

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
     print("branch test")
+    print("branch test2")
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
